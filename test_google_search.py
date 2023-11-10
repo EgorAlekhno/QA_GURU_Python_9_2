e54248b9f55b7e4d5d01edc49cd6675a65466b3e
Egor <77302273+EgorAlekhno@users.noreply.github.com>
2023-11-10 18:35:43 +0300

    Update test_google_search.py

--- test_google_search.py
+++ test_google_search.py
@@ -5,7 +5,6 @@ from selene import be, have
 
 @pytest.fixture
 def setup_browser():
-    # browser.config.window_size = (375, 600)
     browser.config.window_width = 1440
     browser.config.window_height = 900
     browser.open('https://google.com')
@@ -14,6 +13,7 @@ def test_google_should_find_selene_with_pytest(setup_browser):
    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
    browser.quit()
+    
 def test_google_should_not_find_selene_with_pytest(setup_browser):
    browser.element('[name="q"]').should(be.blank).type('wtvwtvbwtrbrttbrwasvasfvasfvdafv').press_enter()
    browser.element('[class="card-section"]').should(have.text('По запросу wtvwtvbwtrbrttbrwasvasfvasfvdafv ничего не найдено.'))
